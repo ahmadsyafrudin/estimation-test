bb98c5c020d32150fb60114443a0ce286c499fdf
Ahmad Syafrudin <ahmad.syafrudin@gramedia.digital>
2019-05-10 01:37:40 +0700

    validate entity that send to API from request body

--- estimation/views.py
+++ estimation/views.py
@@ -10,22 +10,34 @@ from estimation.helpers import Estimation
 
 @csrf_exempt
 def estimate(request):
+    try:
+        date_time = json.loads(request.body)["dateTime"]
+        estimation_type = json.loads(request.body)["estimationType"]
+
+    except Exception as e:
+        return JsonResponse(data={"message": f"supported request body is dateTime and estimationType",
+                                  "error": f"{type(e)} {json.loads(request.body).keys()}"},
+                            status=HTTPStatus.BAD_REQUEST)
+
     if request.method == "POST":
 
-        estimation = Estimation(json.loads(request.body).get("dateTime"),
-                                json.loads(request.body).get("estimationType"))
-        pickup = True if json.loads(request.body).get("estimationType") == "return" else None
+        estimation = Estimation(date_time,
+                                estimation_type)
+        pickup = True if estimation_type == "return" else None
         allowed, estimated, reason = estimation.estimate(pickup=pickup)
-        response = {"delivery": estimation.delivery,
-                    "return": estimation.pick_up
-                    }
+        response = {
+            "delivery": estimation.delivery,
+            "return": estimation.pick_up
+        }
         if allowed:
-            return JsonResponse(data=response.get(json.loads(request.body).get("estimationType"))(),
+            return JsonResponse(data=response.get(estimation_type)(),
                                 status=HTTPStatus.OK)
+
         estimated = estimated.first().date.strftime("%A") if isinstance(estimated, QuerySet) else estimated.strftime(
             "%A")
 
-        return JsonResponse(data={"message": f"can't order on {estimated}, because {reason}"},
+        return JsonResponse(data={"message": f"can't estimate on {estimated}, because {reason}"},
                             status=HTTPStatus.BAD_REQUEST)
+
     else:
         return JsonResponse(data={"message": "method not allowed"}, status=HTTPStatus.METHOD_NOT_ALLOWED)
