d1cb8a7b899749a6e3b65d7c7785318db3e013e9
Ahmad Syafrudin <sfrmd4@gmail.com>
2019-10-07 15:14:24 +0700

    rename file tests

--- tests/features/environment.py
+++ tests/features/environment.py
@@ -1,12 +1,12 @@
 from behave import use_fixture
 from django.core.management import call_command
 
-from test.features.behave_fixtures import django_test_runner, django_test_case
+from tests.features.behave_fixtures import django_test_runner, django_test_case
 
 
 def before_all(context):
     use_fixture(django_test_runner, context)
-    call_command('loaddata', "test/fixtures/holiday")
+    call_command('loaddata', "tests/fixtures/holiday")
 
 
 def before_scenario(context, scenario):
